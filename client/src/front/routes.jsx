1ac314327651cddecb8198846f78b379a645d292
luowenstar <1291962779@qq.com>
2019-03-20 17:34:35 +0800

    ....

--- client/src/front/routes.jsx
+++ client/src/front/routes.jsx
@@ -7,20 +7,10 @@ import Register from './pages/register';
 import Article from './pages/article';
 import Person from './pages/person';
 
-// const PrivateRouter = () => {
-// 	console.log(component)
-// 	// <Route
-// 	// 	{...rest}
-// 	// 	render={(props) => {
-// 	// 		console.log(props.localtion);
-// 	// 		sessionStorage.getItem('token') ? (
-// 	// 			<Component {...props} />
-// 	// 		) : (
-// 	// 			<Redirect to={{ path: 'login', state: { from: props.localtion } }} />
-// 	// 		);
-// 	// 	}}
-// 	// />;
-// };
+
+const Enter =(nextState,replace)=> {
+	console.log('进入这个界面')
+}
 
 export default (
 	<Router history={hashHistory}>
@@ -30,7 +20,8 @@ export default (
 			<Route path="article" component={Article} />
 			<Route path="login" component={Login} />
 			<Route path="register" component={Register} />
-			<Route path='person' component={Person} />
+			<Route path='person' component={Person} onEnter={Enter} />
 		</Route>
 	</Router>
 );
+
