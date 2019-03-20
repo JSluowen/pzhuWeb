dce9a5ed43c6f1d19261baab9a8fb3410d75dacf
luowenstar <1291962779@qq.com>
2019-03-20 17:08:55 +0800

    feat:person

--- client/src/front/routes.jsx
+++ client/src/front/routes.jsx
@@ -1,48 +1,26 @@
 import * as React from 'react';
-import { Router, Route, hashHistory, IndexRedirect } from 'react-router';
+import { Router, Route, hashHistory, IndexRedirect, Redirect } from 'react-router';
 import Layout from './layout/index';
 import Home from './pages/home';
 import Login from './pages/login';
 import Register from './pages/register';
 import Article from './pages/article';
+import Person from './pages/person';
 
-// const routers = [
-// 	{
-// 		path: '/',
-// 		component: Layout,
-// 		children: [
-// 			{
-// 				indexRedirect: true,
-// 				to: 'index',
-// 				component: Layout
-// 			},
-// 			{
-// 				path: 'article',
-// 				component: Article,
-// 				requiresAuth: true
-// 			},
-// 			{
-// 				path: 'login',
-// 				component: Login
-// 			}
-// 		]
-// 	}
-// ];
-//  const renderRouter=(router)=> {
-// 	<Switch>
-// 		{router.map((item, index) => {
-// 			if (item.IndexRedirect) {
-// 				<IndexRedirect to={item.to} />;
-// 			} else {
-// 				<Route key={index} path={item.path} component={item.component}>
-// 					{item.children ? renderRouter(item.children) : ''}
-// 				</Route>;
-// 			}
-// 		})}
-// 	</Switch>;
-// }
-
-//  export default <Router history={hashHistory}>{renderRouter(routers)}</Router>;
+// const PrivateRouter = () => {
+// 	console.log(component)
+// 	// <Route
+// 	// 	{...rest}
+// 	// 	render={(props) => {
+// 	// 		console.log(props.localtion);
+// 	// 		sessionStorage.getItem('token') ? (
+// 	// 			<Component {...props} />
+// 	// 		) : (
+// 	// 			<Redirect to={{ path: 'login', state: { from: props.localtion } }} />
+// 	// 		);
+// 	// 	}}
+// 	// />;
+// };
 
 export default (
 	<Router history={hashHistory}>
@@ -51,7 +29,8 @@ export default (
 			<Route path="index" component={Home} />
 			<Route path="article" component={Article} />
 			<Route path="login" component={Login} />
-			<Route path="register" component={Register} />>
+			<Route path="register" component={Register} />
+			<Route path='person' component={Person} />
 		</Route>
 	</Router>
 );
