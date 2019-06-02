b161360fbfbd01a8e3345b223d70e3e3270d4056
luowenstar <1291962779@qq.com>
2019-06-02 15:19:46 +0800

    feat:搭建资源管理界面

---
+++ client/src/back/pages/resource/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './index.scss';
+class Resource extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <div className='backResource'>
+                
+            </div>
+        );
+    }
+}
+
+export default Resource;
