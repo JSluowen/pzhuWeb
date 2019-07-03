db931bfaf54d21a4f7b11b49e3359b8472d58687
luowenstar <1291962779@qq.com>
2019-07-03 10:57:56 +0800

    fix: front page  bug

--- client/src/front/pages/articleEdit/index.jsx
+++ client/src/front/pages/articleEdit/index.jsx
@@ -14,11 +14,33 @@ import 'braft-editor/dist/index.css'
 // 代码高亮显示
 import 'braft-extensions/dist/code-highlighter.css'
 import './index.scss'
+//引入MakeDown语法
+import Markdown from 'braft-extensions/dist/markdown'
 import moment from 'moment';
 const dateFormat = 'YYYY-MM-DD';
-BraftEditor.use(CodeHighlighter({
+let options = {
 	includeEditors: ['editor-with-code-highlighter'],
-}))
+	syntaxs: [
+		{
+			name: 'JavaScript',
+			syntax: 'javascript'
+		}, {
+			name: 'HTML',
+			syntax: 'html'
+		}, {
+			name: 'CSS',
+			syntax: 'css'
+		}, {
+			name: 'Java',
+			syntax: 'java',
+		}, {
+			name: 'PHP',
+			syntax: 'php'
+		}
+	]
+}
+BraftEditor.use(CodeHighlighter(options));
+BraftEditor.use(Markdown(options))
 
 class ArticleEdit extends Component {
 	constructor(props) {
