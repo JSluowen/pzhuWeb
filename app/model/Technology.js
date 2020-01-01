ec67a0364773217ec889eb587ce7174d4dcf7f1d
luowen <luowen.lw@bytedance.com>
2020-01-01 15:35:34 +0800

    feat: 优化界面

--- app/model/Technology.js
+++ app/model/Technology.js
@@ -1,28 +1,31 @@
 'use strict';
 module.exports = app => {
-    const { INTEGER, STRING } = app.Sequelize;
-    const Technology = app.model.define('Technology', {
-        id: {
-            type: INTEGER(10),
-            primaryKey: true,
-            autoIncrement: true
-        },
-        name: {
-            type: STRING(16),
-        },
-        status: {
-            type: INTEGER(4),
-            defaultValue: 1,
-        }
+  const { INTEGER, STRING } = app.Sequelize;
+  const Technology = app.model.define(
+    'Technology',
+    {
+      id: {
+        type: INTEGER(10),
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: STRING(16),
+      },
+      status: {
+        type: INTEGER(4),
+        defaultValue: 1,
+      },
     },
     {
-        timestamps: false,
-        underscored: true,
-        tableName: 'technology',
-    });
-    Technology.associate = function() {
-        app.model.Technology.hasMany(app.model.Article, { foreignKey: 'technologyid', targetKey: 'id' });
-    };
+      timestamps: false,
+      underscored: true,
+      tableName: 'technology',
+    },
+  );
+  Technology.associate = function() {
+    app.model.Technology.hasMany(app.model.Article, { foreignKey: 'technologyid', targetKey: 'id' });
+  };
 
-    return Technology;
+  return Technology;
 };
