d634aef0c92a7daf5525e7539e9e75bf5b4499ed
luowen <luowen.lw@bytedance.com>
2020-01-18 19:19:38 +0800

    feat: 配置stylint的检查

--- client/src/front/pages/achievement/index.jsx
+++ client/src/front/pages/achievement/index.jsx
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
-import { Icon, Card, Button, Avatar, Row, Col, Input, Skeleton, message } from 'antd'
-import './index.scss'
-import AchievementAPI from '../../api/achievement'
+import React, { Component } from 'react';
+import { Icon, Card, Button, Avatar, Row, Col, Input, Skeleton, message } from 'antd';
+import './index.scss';
+import AchievementAPI from '../../api/achievement';
 const { Meta } = Card;
 const Search = Input.Search;
 class Achievement extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      limit: 6,// 获取的数据量
+      limit: 6, // 获取的数据量
       beg: 0,
       end: 6,
       loading: true,
@@ -21,52 +21,57 @@ class Achievement extends Component {
     this.achievementTypeRef = React.createRef();
   }
   componentDidMount() {
-    this.getAchievement()
-    window.addEventListener('scroll', this.handelScroll)
+    this.getAchievement();
+    window.addEventListener('scroll', this.handelScroll);
   }
-  handelScroll = (e) => {
+  handelScroll = e => {
     // 滚动的高度
-    const scrollTop = (event.srcElement ? event.srcElement.documentElement.scrollTop : false) || window.pageYOffset || (event.srcElement ? event.srcElement.body.scrollTop : 0);
+    const scrollTop =
+      (event.srcElement ? event.srcElement.documentElement.scrollTop : false) ||
+      window.pageYOffset ||
+      (event.srcElement ? event.srcElement.body.scrollTop : 0);
     // 视窗高度
-    const clientHeight = (event.srcElement && event.srcElement.documentElement.clientHeight) || document.body.clientHeight;
+    const clientHeight =
+      (event.srcElement && event.srcElement.documentElement.clientHeight) || document.body.clientHeight;
     // 页面高度
-    const scrollHeight = (event.srcElement && event.srcElement.documentElement.scrollHeight) || document.body.scrollHeight;
+    const scrollHeight =
+      (event.srcElement && event.srcElement.documentElement.scrollHeight) || document.body.scrollHeight;
     // 距离页面底部的高度
     const height = scrollHeight - scrollTop - clientHeight;
     if (height <= 300) {
-      this.handelLoading()
+      this.handelLoading();
     }
-  }
+  };
   handelLoading = () => {
     if (this.state.isLoading) {
       this.setState({
         isLoading: false,
         beg: this.state.end,
-        end: this.state.end + this.state.limit
-      })
-      this.getAchievement()
+        end: this.state.end + this.state.limit,
+      });
+      this.getAchievement();
     }
-  }
+  };
   // 设置初始化的资源分类
   setAchievementTyep = () => {
     let e = this.achievementTypeRef.current;
     e = e.childNodes;
     e[1].classList.add('achievementActive');
     this.setState({
-      flag: false
-    })
-  }
+      flag: false,
+    });
+  };
 
   getAchievement = () => {
-    let params = {
+    const params = {
       beg: this.state.beg,
       end: this.state.end,
-      index: this.state.index
-    }
+      index: this.state.index,
+    };
     AchievementAPI.getAachievement(params).then(res => {
-      let arry = this.state.ac;
-      for (let item of res.data.ac) {
-        arry.push(item)
+      const arry = this.state.ac;
+      for (const item of res.data.ac) {
+        arry.push(item);
       }
       if (res.success) {
         setTimeout(() => {
@@ -74,164 +79,169 @@ class Achievement extends Component {
             loading: false,
             acType: res.data.acType,
             ac: arry,
-            isLoading: true
-          })
+            isLoading: true,
+          });
           if (this.state.flag) {
             this.setAchievementTyep();
           }
-        }, 500)
+        }, 500);
       } else {
         setTimeout(() => {
           this.setState({
             acType: res.data.acType,
             ac: arry,
             loading: false,
-            isLoading: false
-          })
+            isLoading: false,
+          });
           if (this.state.flag) {
             this.setAchievementTyep();
           }
-        }, 500)
+        }, 500);
       }
-    })
-  }
+    });
+  };
   // 筛选成果类别
-  filterAchievement = (e) => {
-    let event
-    if (e.target.tagName == "DIV") {
-      event = e.target
+  filterAchievement = e => {
+    let event;
+    if (e.target.tagName === 'DIV') {
+      event = e.target;
     } else {
-      event = e.target.parentNode
+      event = e.target.parentNode;
     }
-    let parent = event.parentNode.children
+    const parent = event.parentNode.children;
     for (let i = 0; i < parent.length; i++) {
-      parent[i].classList.remove("achievementActive");
+      parent[i].classList.remove('achievementActive');
     }
-    event.classList.add('achievementActive')
-    let index = event.getAttribute('index')
+    event.classList.add('achievementActive');
+    const index = event.getAttribute('index');
 
     if (this.state.index === parseInt(index)) {
-      return
-    } else {
-      this.setState({
+      return;
+    }
+    this.setState(
+      {
         index: parseInt(index),
         flag: false,
         loading: true,
         beg: 0,
         end: this.state.limit,
-        ac: []
-      }, () => {
+        ac: [],
+      },
+      () => {
         this.getAchievement();
-      })
-    }
-
-  }
+      },
+    );
+  };
   // 搜索资源
-  handelSerach = (value) => {
+  handelSerach = value => {
     if (value === '') {
-      message.warning('请先输入成果标题')
+      message.warning('请先输入成果标题');
       return;
     }
-    let params = {
-      value: value
-    }
+    const params = {
+      value,
+    };
     this.setState({
-      loading: true
-    })
+      loading: true,
+    });
     AchievementAPI.searchAchievement(params).then(res => {
       if (res.success) {
         setTimeout(() => {
           this.setState({
             loading: false,
-            ac: res.data
-          })
-        }, 500)
-        message.success('搜索成功')
+            ac: res.data,
+          });
+        }, 500);
+        message.success('搜索成功');
       } else {
         setTimeout(() => {
           this.setState({
-            loading: false
-          })
-        }, 500)
-        message.warning('为搜索到您想要的资源')
+            loading: false,
+          });
+        }, 500);
+        message.warning('为搜索到您想要的资源');
       }
-    })
-  }
+    });
+  };
 
   render() {
     return (
-      <div className='achievement'>
-        <div className='achievement-left' ref={this.achievementTypeRef}>
-          <div className='achievement-left-header'>
-            成果分类
-                    </div>
-          <div className='achievement-left-item' index="0" key="0" onClick={this.filterAchievement} >
+      <div className="achievement">
+        <div className="achievement-left" ref={this.achievementTypeRef}>
+          <div className="achievement-left-header">成果分类</div>
+          <div className="achievement-left-item" index="0" key="0" onClick={this.filterAchievement}>
             <p>全部</p>
           </div>
-          {
-            this.state.acType.map(item => {
-              return <div className='achievement-left-item' index={item.id} key={item.id} onClick={this.filterAchievement} >
+          {this.state.acType.map(item => {
+            return (
+              <div className="achievement-left-item" index={item.id} key={item.id} onClick={this.filterAchievement}>
                 <p> {item.name}</p>
                 <p>{item.index}</p>
               </div>
-
-            })
-          }
+            );
+          })}
         </div>
-        <div className='achievement-right'>
+        <div className="achievement-right">
           <Card
             title={this.state.acType.map(item => {
               if (item.id === this.state.index) {
-                return item.name
+                return item.name;
               }
             })}
             style={{ width: '100%' }}
-            extra={<Search
-              placeholder="请输入成果标题"
-              onSearch={value => this.handelSerach(value)}
-              style={{ width: 200 }}
-            />}
+            extra={
+              <Search
+                placeholder="请输入成果标题"
+                onSearch={value => this.handelSerach(value)}
+                style={{ width: 200 }}
+              />
+            }
           >
             <Skeleton loading={this.state.loading} active>
               <Row style={{ width: '100%', margin: 0 }} gutter={16}>
-                {
-                  this.state.ac.length === 0 ?
-                    <div className='achievement-right-null'>
-                      暂无数据
-                                        </div>
-                    :
-                    <div>
-                      {
-                        this.state.ac.map(item => {
-                          return <Col span={12} key={item.id} >
-                            <a style={{ display: 'block' }} href={item.achievementlink || item.attachment} target='_blank'>
-                              <Card
-                                className="achievement-right-item"
-                                hoverable={true}
-                                loading={false}
+                {this.state.ac.length === 0 ? (
+                  <div className="achievement-right-null">暂无数据</div>
+                ) : (
+                  <div>
+                    {this.state.ac.map(item => {
+                      return (
+                        <Col span={12} key={item.id}>
+                          <a
+                            style={{ display: 'block' }}
+                            href={item.achievementlink || item.attachment}
+                            target="_blank"
+                          >
+                            <Card
+                              className="achievement-right-item"
+                              hoverable={true}
+                              loading={false}
+                              style={{ width: '100%' }}
+                              cover={<img alt="example" src={item.posterlink} />}
+                              actions={[
+                                <span>
+                                  <Icon type="user" /> {item.UserInfo.User.name}
+                                </span>,
+                                <span>
+                                  <Icon type="calendar" /> {item.created_at}
+                                </span>,
+                              ]}
+                            >
+                              <Meta
                                 style={{ width: '100%' }}
-                                cover={<img alt="example" src={item.posterlink} />}
-                                actions={[<span><Icon type="user" />{" "}{item.UserInfo.User.name}</span>, <span><Icon type="calendar" />{" "}{item.created_at}</span>]}
-                              >
-                                <Meta
-                                  style={{ width: '100%' }}
-                                  avatar={<Avatar src={item.UserInfo.avatar} />}
-                                  title={item.title}
-                                  description={item.abstract}
-                                />
-                              </Card>
-                            </a>
-                          </Col>
-                        })
-                      }
-                    </div>
-                }
+                                avatar={<Avatar src={item.UserInfo.avatar} />}
+                                title={item.title}
+                                description={item.abstract}
+                              />
+                            </Card>
+                          </a>
+                        </Col>
+                      );
+                    })}
+                  </div>
+                )}
               </Row>
-
             </Skeleton>
-
           </Card>
-
         </div>
       </div>
     );
