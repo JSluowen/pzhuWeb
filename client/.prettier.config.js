43b86d11cd00bcee948ce13ef8da21a4f84c1763
alphago <2774598495@qq.com>
2021-03-02 12:24:41 +0800

    feat: 完成上传等功能

--- client/.prettier.config.js
+++ client/.prettier.config.js
@@ -2,11 +2,11 @@ module.exports = {
   // 一行最多 100 字符
   printWidth: 100,
   // 使用 4 个空格缩进
-  tabWidth: 4,
+  tabWidth: 2,
   // 不使用缩进符，而使用空格
   useTabs: false,
   // 行尾需要有分号
-  semi: true,
+  semi: false,
   // 使用单引号
   singleQuote: true,
   // 对象的 key 仅在必要时用引号
